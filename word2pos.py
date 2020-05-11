1d9c5b288af6abfc06f64241627c2c589d2f0049
Getman Yaroslav <getmany1@login2.triton.aalto.fi>
2020-05-11 15:00:17 +0300

    word2pos

--- word2pos.py
+++ word2pos.py
@@ -9,6 +9,10 @@ def word2pos(corpus, pos_tagger):
     # Read the corpus file
     with open(os.path.join('data','corpora', corpus), encoding='utf8') as f:
         text = f.read()
+
+    # Load the POS tagger
+    with open(os.path.join('data', 'models', pos_tagger), 'rb') as f:
+        pos_tagger = pickle.load(f)
         
     # Tokenize
     text = sent_tokenize(text)
